91c0aa6628c1d870639e3e6ef95a72f5f08cc874
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-10-04 14:06:41 -0400

    changed locations of paths

--- refassem.sh
+++ refassem.sh
@@ -2,23 +2,20 @@
 
 cd /escratch4/s_11/s_11_Aug_17/refassem
 
-time /usr/local/bwa/latest/bwa
-time /usr/local/samtools/latest/bin/samtools
 
 # download reference genome and unzip reference genome
 wget -q -o ref.fa.gz ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz
-
 
 # download reads
 wget -q http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/s_6_1.fastq.gz
 wget -q http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/s_6_2.fastq.gz
 
-
 #unzip reference genome
 gunzip -c Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz > ref.fa
 
 
-
+cd /escratch4/s_11/s_11_Aug_17/refassem
+time /usr/local/bwa/latest/bwa
 
 # create index of reference genome for BWA
 bwa index ref.fa
@@ -28,6 +25,8 @@ bwa mem -t 4 ref.fa s_6_1.fastq.gz s_6_2.fastq.gz | samtools view -b - > aln.bam
 
 
 
+cd /escratch4/s_11/s_11_Aug_17/refassem
+time /usr/local/samtools/latest/bin/samtools
 
 # sort BAM file
 samtools sort -@ 4 -O bam -T tmp aln.bam > aln.sort.bam
