8f54883884cc0a0d7de1b4148379f4b26dd3ebd5
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-10-04 17:07:22 -0400

    added correct path to programs

--- refassem.sh
+++ refassem.sh
@@ -2,8 +2,7 @@
 
 cd /escratch4/s_11/s_11_Aug_17/refassem
 
-time /usr/local/samtools/latest/bin/samtools
-time /usr/local/bwa/latest/bwa
+export PATH=/path/to/bwa:/path/to/samtools:/path/to/bcftools:$PATH
 
 # download reference genome and unzip reference genome
 wget -q -O ref.fa.gz ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz
