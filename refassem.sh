62929f8ebf690fbf6e23099f5d0cd162772165ba
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-10-04 13:41:54 -0400

    changed BWA program path call

--- refassem.sh
+++ refassem.sh
@@ -2,11 +2,9 @@
 
 cd /escratch4/s_11/s_11_Aug_17/refassem
 
-time /usr/local/bwa/latest/bwa
-/usr/local/bwa/latest/bwa index -p hg19 -a bwtsw hg19.fa
+time /usr/local/bwa/latest/bwa index -p hg19 -a bwtsw hg19.fa
 
 time /usr/local/samtools/latest/bin/samtools
-
 
 # download reference genome and unzip reference genome
 wget -q -o ref.fa.gz ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz
@@ -19,6 +17,7 @@ wget -q http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/s_6_2.fastq.g
 
 #unzip reference genome
 gunzip -c Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz > ref.fa
+
 
 # create index of reference genome for BWA
 bwa index ref.fa
