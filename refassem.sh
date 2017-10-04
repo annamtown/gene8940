6211a1d6236b2887d001ba8f8fc62e26f5b4d94a
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-10-04 13:26:41 -0400

    Changed gunzip command placement

--- refassem.sh
+++ refassem.sh
@@ -7,7 +7,7 @@ time /usr/local/samtools/latest/bin/samtools
 
 
 # download reference genome and unzip reference genome
-wget -q -o ref.fa.gz ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz | gunzip -c > ecoli_MG1655.fa
+wget -q -o ref.fa.gz ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz
 
 
 # download reads
@@ -15,12 +15,15 @@ wget -q http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/s_6_1.fastq.g
 wget -q http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/s_6_2.fastq.gz
 
 
+#unzip reference genome
+gunzip -c Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz > ref.fa
+
 # create index of reference genome for BWA
-bwa index ecoli_MG1655.fa
+bwa index ref.fa
 
 
 # map reads to reference with BWA & output as BAM file
-bwa mem -t 4 ecoli_MG1655.fa s_6_1.fastq.gz s_6_2.fastq.gz | samtools view -b - > aln.bam
+bwa mem -t 4 ref.fa s_6_1.fastq.gz s_6_2.fastq.gz | samtools view -b - > aln.bam
 
 
 # sort BAM file
@@ -32,7 +35,7 @@ samtools index aln.sort.bam
 
 
 # generate genotype likelihoods with `samtools mpileup` and base calls with `bcftools call`, output as VCF
-samtools mpileup -u -f ecoli_MG1655.fa aln.sort.bam | bcftools call -c -v -O z -o aln.sort.vcf.gz
+samtools mpileup -u -f ref.fa aln.sort.bam | bcftools call -c -v -O z -o aln.sort.vcf.gz
 
 
 # index VCF file
@@ -40,4 +43,4 @@ bcftools index aln.sort.vcf.gz
 
 
 # generate consensus sequence
-bcftools consensus -f ecoli_MG1655.fa aln.sort.vcf.gz > consensus.fa
+bcftools consensus -f ref.fa aln.sort.vcf.gz > consensus.fa
