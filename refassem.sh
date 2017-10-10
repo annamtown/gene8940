63ead60becf8613023f3c0e0743f2e04de6fed0d
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-10-09 21:55:12 -0400

    changed samtools command

--- refassem.sh
+++ refassem.sh
@@ -2,8 +2,6 @@
 
 cd /escratch4/s_11/s_11_Aug_17/refassem
 
-
-
 # download reference genome and unzip reference genome
 wget -q -O ref.fa.gz ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz
 
