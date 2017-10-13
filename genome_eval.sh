e14262a1ba4818e9536bb3cff33d73a41c3fce78
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-10-12 20:04:40 -0400

    First "completed" script draft

--- genome_eval.sh
+++ genome_eval.sh
@@ -3,16 +3,14 @@
 cd /escratch4/s_11/s_11_Aug_17/genome_eval
 
 # put the mummer executables in your $PATH
-export PATH=/usr/local/mummer/latest/mummer/:$PATH
+export PATH=/usr/local/mummer/3.22/:$PATH
 
 # put the Prokka executable and its dependencies in your $PATH
-export PATH=/usr/local/prokka/1.11/:/usr/local/hmmer/2.3.2/:/usr/local/rnammer/latest/:/usr/local/tbl2asn/01052015:/usr/local/signalp/4.1c/:/usr/local/parallel/20150822/:$PATH
-
+export PATH=/usr/local/prokka/1.11/:/usr/local/hmmer/2.3.2/:/usr/local/rnammer/1.2/:/usr/local/tbl2asn/01052015/:/usr/local/signalp/4.1c/:/usr/local/parallel/20150822/:$PATH
 
 
 # download Ensembl MG1655 reference genome
-wget -q -O ref_ecoli.fa.gz ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/
-Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz
+wget -q -O ref_ecoli.fa.gz ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz
 
 #unzip reference genome
 gunzip -c ref_ecoli.fa.gz > ref_ecoli.fa
@@ -21,15 +19,34 @@ gunzip -c ref_ecoli.fa.gz > ref_ecoli.fa
 wget -q http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/scaffolds.fasta
 
 # run QUAST 3.1 on reference-based, Pacbio and Spades assemblies using Ensembl MG1655 as reference
-python2.7 /usr/local/quast/3.1/quast.py -o /escratch4/s_11/s_11_Aug_17/genome_eval/quast_output -R /escratch4/s_11/s_11_Aug_17/genome_eval/ref_ecoli.fa /escratch4/s_11/s_11_Aug_17/genome_eval/samtools_consensus.fa /escratch4/s_11/s_11_Aug_17/genome_eval/pacbio_contigs.fasta
-/escratch4/s_11/s_11_Aug_17/genome_eval/spades_scaffolds.fasta
+python2.7 /usr/local/quast/3.1/quast.py -o /escratch4/s_11/s_11_Aug_17/genome_eval/quast_output -R /escratch4/s_11/s_11_Aug_17/genome_eval/ref_ecoli.fa /escratch4/s_11/s_11_Aug_17/refassem/consensus.fa /escratch4/s_11/s_11_Aug_17/ecoli/ecoli-pacbio/ecoli.contigs.fasta /escratch4/s_11/s_11_Aug_17/genome_eval/scaffolds.fasta
 
 # make mummerplots for reference-based, Pacbio and Spades assemblies using Ensembl MG1655 as reference
 # need to run the 3 following lines for each assembly
-nucmer -o /escratch4/s_11/s_11_Aug_17/genome_eval/ref_ecoli.fa /escratch4/s_11/s_11_Aug_17/genome_eval -p outputfile_prefix
-delta-filter -1 outputfile_prefix.delta > outputfile_prefix.1delta
-mummerplot --size large -fat --color -f --png outputfile_prefix.1delta -p outputfile_prefix
+#reference based (refassem)
+nucmer -o /escratch4/s_11/s_11_Aug_17/genome_eval/ref_ecoli.fa /escratch4/s_11/s_11_Aug_17/refassem/consensus.fa -p outputref_prefix
+delta-filter -1 outputref_prefix.delta > outputref_prefix.1delta
+mummerplot --size large -fat --color -f --png outputref_prefix.1delta -p outputref_prefix
+
+#pacbio (canu)
+nucmer -o /escratch4/s_11/s_11_Aug_17/genome_eval/ref_ecoli.fa /escratch4/s_11/s_11_Aug_17/ecoli/ecoli-pacbio/ecoli.contigs.fasta -p outputpacbio_prefix
+delta-filter -1 outputpacbio_prefix.delta > outputpacbio_prefix.1delta
+mummerplot --size large -fat --color -f --png outputpacbio_prefix.1delta -p outputpacbio_prefix
+
+#spades
+nucmer -o /escratch4/s_11/s_11_Aug_17/genome_eval/ref_ecoli.fa /escratch4/s_11/s_11_Aug_17/genome_eval/scaffolds.fasta -p outputspades_prefix
+delta-filter -1 outputspades_prefix.delta > outputspades_prefix.1delta
+mummerplot --size large -fat --color -f --png outputspades_prefix.1delta -p outputspades_prefix
 
 # generate Prokka genome annotations for reference-based, Pacbio and Spades assemblies using Ensembl MG1655 as reference
-# need to run the 3 following lines for each assembly
-prokka /escratch4/s_11/s_11_Aug_17/genome_eval --outdir prokka_directory
+# need to run the following lines for each assembly
+#reference-based
+prokka /escratch4/s_11/s_11_Aug_17/refassem/consensus.fa --outdir prokka_directory
+
+#pacbio
+prokka /escratch4/s_11/s_11_Aug_17/ecoli/ecoli-pacbio/ecoli.contigs.fasta --outdir prokka_directory
+
+#spades
+prokka /escratch4/s_11/s_11_Aug_17/genome_eval/scaffolds.fasta --outdir prokka_directory
+
+#clean up script by removing files
