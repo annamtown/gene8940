ed4fdb1181e6e05f43903e0c9baf71342a32e0aa
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-10-24 13:32:30 -0400

    changed path

--- canuscript.sh
+++ canuscript.sh
@@ -2,9 +2,9 @@
 
 cd /escratch4/s_11/s_11_Aug_17/ecoli
 
-export PATH=/usr/local/canu/1.4/Linux-amd64/bin:/usr/local/java/jdk1.8.0_74/bin:${PATH}
-export LD_LIBRARY_PATH=/usr/local/gcc/5.3.0/lib64:${LD_LIBRARY_PATH}
-export JAVA_HOME=/usr/local/java/jdk1.8.0_74
+export PATH=/usr/local/canu/1.4/Linux-amd64/bin:/usr/local/java/jdk1.8.0_74/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/gcc/5.3.0/lib64:$PATH
+export JAVA_HOME=/usr/local/java/jdk1.8.0_74:$PATH
 
 curl -L -o pacbio.fastq http://gembox.cbcb.umd.edu/mhap/raw/ecoli_p6_25x.filtered.fastq
 
