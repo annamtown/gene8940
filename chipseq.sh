b375516e34efadf6b03552c5dbc78a90b248975e
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-10-31 10:00:13 -0400

    added .fai back to first bedtools

--- chipseq.sh
+++ chipseq.sh
@@ -49,7 +49,7 @@ macs14 -t chip.bam -c input.bam -f BAM -g 4641652 -n "FNR" --bw=400 --keep-dup=1
 samtools faidx ref.fa
 
 # add ±100 bp to the FNR_summits.bed using `bedtools slop`
-bedtools slop -b 100 -i FNR_summits.bed -g ref.fa > FNR_summits_100.bed
+bedtools slop -b 100 -i FNR_summits.bed -g ref.fa.fai > FNR_summits_100.bed
 
 #extract fasta sequences for ±100 bp FNR_summits using `bedtools getfasta`
 bedtools getfasta -fo FNR_summits_100.fa -fi ref.fa -bed FNR_summits_100.bed
