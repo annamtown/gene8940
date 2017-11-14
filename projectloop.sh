8db264a04525cb924c02e741fe86bddb0c635399
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-11-14 16:16:06 -0500

    added path for RAxML

--- projectloop.sh
+++ projectloop.sh
@@ -7,6 +7,9 @@ cd /escratch4/s_11/s_11_Aug_17/project
 export PATH=/usr/local/samtools/1.2/:$PATH
 export PATH=/usr/local/bwa/0.7.10/:$PATH
 
+# path for RAxML
+export PATH=/usr/local/raxml/8.2.4/:$PATH
+ 
 # get reference genome
 wget -q -O ref.fa.gz ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/fasta/bacteria_4_collection/salmonella_enterica_subsp_enterica_serovar_enteritidis_str_p125109/dna/Salmonella_enterica_subsp_enterica_serovar_enteritidis_str_p125109.ASM950v1.dna.chromosome.Chromosome.fa.gz
 curl -s "https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=PRJEB634&result=read_run&fields=study_accession,sample_accession,secondary_sample_accession,sample_alias,experiment_accession,run_accession,scientific_name,instrument_model,library_layout,library_source,library_selection,read_count,base_count,experiment_title,fastq_ftp" | grep -v PacBio > meta.tsv
