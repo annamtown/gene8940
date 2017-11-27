c5796dedb83c481c46bcc6ee21681085e2cd9e88
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-11-27 16:36:52 -0500

    removed gtr flag

--- fasttree.sh
+++ fasttree.sh
@@ -7,4 +7,4 @@ cd /escratch4/s_11/s_11_Aug_17/fasttree
 #path for fasttree
 export PATH=/usr/local/fasttree/2.1.4/:$PATH
 
-FastTree -gtr -nt < /escratch4/s_11/s_11_Aug_17/project/allconsensus.phy > fasttree_file
+FastTree -nt < /escratch4/s_11/s_11_Aug_17/project/allconsensus.phy > fasttree_file
