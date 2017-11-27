c60d59707920f8f2c7c8ee6e33057e66d7490781
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-11-27 17:47:07 -0500

    added -n flag

--- fasttree.sh
+++ fasttree.sh
@@ -7,4 +7,4 @@ cd /escratch4/s_11/s_11_Aug_17/fasttree
 #path for fasttree
 export PATH=/usr/local/fasttree/2.1.4/:$PATH
 
-FastTree -nt < /escratch4/s_11/s_11_Aug_17/project/allconsensus.phy > fasttree_file
+FastTree -nt -n 10 < /escratch4/s_11/s_11_Aug_17/project/allconsensus.phy > fasttree_file
