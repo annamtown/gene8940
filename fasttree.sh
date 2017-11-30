85a46fb68107ad17bad6fb5815fd8e1f35b02a60
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2017-11-30 17:03:31 -0500

    added command for ERR files

--- fasttree.sh
+++ fasttree.sh
@@ -5,4 +5,4 @@ cd /escratch4/s_11/s_11_Aug_17/fasttree
 #path for fasttree
 export PATH=/usr/local/fasttree/2.1.4/:$PATH
 
-FastTree -nt -gtr < /escratch4/s_11/s_11_Aug_17/project/allconsensus.phy > fasttree_file
+FastTree -nt -gtr < /escratch4/s_11/s_11_Aug_17/project2/allconsensus.phy > fasttree_file
